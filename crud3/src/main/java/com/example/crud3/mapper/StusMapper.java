5818accda15e489dd736792c3b8aadeb8889a3fe
18thlsx <93168043+18thlsx@users.noreply.github.com>
2023-07-04 09:54:49 +0800

    Delete crud3 directory

--- crud3/src/main/java/com/example/crud3/mapper/StusMapper.java
+++
@@ -1,9 +0,0 @@
-package com.example.crud3.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.example.crud3.entity.StusEntity;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface StusMapper extends BaseMapper<StusEntity> {
-}
--- crud3/src/main/java/com/example/crud3/service/StusService.java
+++
@@ -1,7 +0,0 @@
-package com.example.crud3.service;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import com.example.crud3.entity.StusEntity;
-
-public interface StusService extends IService<StusEntity> {
-}
