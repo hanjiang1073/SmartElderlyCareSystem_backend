06ac19a2f230a8dc3098299c574f593ac6dbaacf
18thlsx <2798252903@qq.com>
2023-07-14 11:43:21 +0800

    4

--- src/main/java/com/example/crud3/controller/SseEmitterController.java
+++ src/main/java/com/example/crud3/controller/SseEmitterController.java
@@ -1,7 +1,12 @@
 package com.example.crud3.controller;
 
+import com.example.crud3.entity.Connect3RequestEntity;
+import com.example.crud3.entity.ElderEntity;
+import com.example.crud3.entity.VolunteerEntity;
 import com.example.crud3.py.Python;
+import com.example.crud3.service.ElderService;
 import com.example.crud3.service.MonggoDB;
+import com.example.crud3.service.VolunteerService;
 import com.example.crud3.service.impl.VideoProcessingThread;
 import com.example.crud3.service.impl.VideoProcessingThread2;
 import com.example.crud3.utils.InitInstance;
@@ -11,12 +16,10 @@ import org.opencv.core.Mat;
 import org.opencv.imgcodecs.Imgcodecs;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
+import javax.annotation.Resource;
 import java.io.File;
 import java.util.Map;
 
@@ -32,6 +35,10 @@ public class SseEmitterController {
     InitInstance initInstance;
 
     Thread thread;
+
+    @Resource
+    VolunteerService volunteerService;
+    ElderService elderService;
 
     @GetMapping("/push/{message}")
     public ResponseEntity<String> push(@PathVariable(name = "message") String message) {
@@ -65,5 +72,50 @@ public class SseEmitterController {
         thread = new VideoProcessingThread2(userId,type);
         thread.start();
         return s;
+    }
+
+    @PostMapping("/connect3")
+    public SseEmitter connect3(@RequestBody Connect3RequestEntity connect3Request){
+        SseEmitter s = SseEmitterServer.connect("53");
+        initInstance = new InitInstance();
+        Python py = new Python();
+        String valorant = null;
+        initInstance.openVideo();
+        if (initInstance.openVideo()) {
+
+                Mat img = initInstance.getMatfromVideo();
+                String tem = initInstance.matToBase64(img);
+                SseEmitterServer.sendMessage("53",tem);
+                //String path = savepath +userId+"\\"+savePicture.getTime();
+                String path = "D:\\frame\\";
+                File file = new File(path);
+                if (!file.exists()) {
+                    Imgcodecs.imwrite(path, img);
+                }
+                Map res = py.pingPython(path + "tmp.jpg", "http://127.0.0.1:5000/faceFeature");
+                //String valorant = initInstance.matToBase64(Imgcodecs.imread((String) res.get("result")));
+                valorant = (String) res.get("result");
+
+
+            }
+
+            if(connect3Request.isType()){
+                ElderEntity elder = new ElderEntity();
+                elder.setName(connect3Request.getName());
+                elder.setAge(connect3Request.getAge());
+                elder.setPhone(connect3Request.getPhone());
+                elder.setDescription(connect3Request.getDesc());
+                elder.setVector(valorant);
+                elderService.save(elder);
+            }else{
+                VolunteerEntity volunteer = new VolunteerEntity();
+                volunteer.setName(connect3Request.getName());
+                volunteer.setAge(connect3Request.getAge());
+                volunteer.setPhone(connect3Request.getPhone());
+                volunteer.setDescription(connect3Request.getDesc());
+                volunteer.setVector(valorant);
+                volunteerService.save(volunteer);
+            }
+            return  s;
     }
 }
---
+++ src/main/java/com/example/crud3/entity/Connect3RequestEntity.java
@@ -0,0 +1,12 @@
+package com.example.crud3.entity;
+
+import lombok.Data;
+
+@Data
+public class Connect3RequestEntity {
+    private boolean type;
+    private String name;
+    private Integer age;
+    private String phone;
+    private String desc;
+}
